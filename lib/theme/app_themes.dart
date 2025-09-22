eeeee40d242e58819ee9c23aecca59cb755b9b92
Warren573 <warren.andrz@gmail.com>
2025-09-22 11:27:50 +0200

    Create AppThemes class with light and dark themes

---
+++ lib/theme/app_themes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppThemes {
+  static final lightTheme = ThemeData.light();
+  static final darkTheme = ThemeData.dark();
+
+  // Ajoute ici tes thèmes personnalisés "fun" ou "sérieux" plus tard
+}
