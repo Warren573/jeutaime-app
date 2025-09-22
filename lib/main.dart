bce11ddde3a861b1fcc1d45d271ae20823a56fbe
Warren573 <warren.andrz@gmail.com>
2025-09-22 14:39:39 +0200

    Refactor main function and remove Firebase setup
    Removed Firebase initialization and adjusted main function.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'routes/app_routes.dart';
 import 'theme/app_themes.dart';
+import 'routes/app_routes.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(JeuTaimeApp());
 }
 
@@ -16,7 +13,6 @@ class JeuTaimeApp extends StatelessWidget {
       title: 'JeuTaime',
       theme: AppThemes.lightTheme,
       darkTheme: AppThemes.darkTheme,
-      themeMode: ThemeMode.system,
       initialRoute: AppRoutes.welcome,
       routes: AppRoutes.routes,
     );
