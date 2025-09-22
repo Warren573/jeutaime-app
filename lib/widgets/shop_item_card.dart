c7e9aee5d2426ca9e57e71e9e6470f37752094a9
Warren573 <warren.andrz@gmail.com>
2025-09-22 11:54:11 +0200

    Update ShopItemCard to use ShopItem properties

--- lib/widgets/shop_item_card.dart
+++ lib/widgets/shop_item_card.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import '../../models/shop_item.dart';
 
 class ShopItemCard extends StatelessWidget {
+  final ShopItem item;
+
+  const ShopItemCard({Key? key, required this.item}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        title: Text('Item boutique'),
-        subtitle: Text('Description'),
+        leading: Icon(item.icon, color: item.color),
+        title: Text(item.name),
+        subtitle: Text(item.description),
+        trailing: Text(item.isRealMoney ? (item.realPrice ?? '') : '${item.price}'),
       ),
     );
   }
